ccdb22353d1a6d137b7afb585d015a0b4f6a80a5
manaltaha555 <mnalth544@gmail.com>
2023-10-05 15:47:45 -0700

    hshshh

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
-    argv = sys.argv[1:]
-    if len(argv) == 0:
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(len(argv)))
-        for i in range(len(argv)):
-            print("{}: {}".format(i + 1, argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
