04046bf4d9bd4389876a1a366b27aba76319adfe
manaltaha555 <mnalth544@gmail.com>
2023-10-11 22:16:30 +0000

    hhshsh

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if my_list == [] or search == None:
-         return None
     count = my_list.count(search)
     if count == 0:
          return my_list
