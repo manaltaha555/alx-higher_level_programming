71a2aaa00d1142fc81c746218390b2bf9db87495
manaltaha555 <mnalth544@gmail.com>
2023-10-11 22:19:00 +0000

    hhshsh

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,9 +3,10 @@
 def search_replace(my_list, search, replace):
     count = my_list.count(search)
     if count == 0:
-         return my_list
+        return my_list
     new = my_list.copy()
     for i in range(len(new)):
-         if new[i] == search:
+        if new[i] == search:
             new[i] = replace
-    return new            
+    return new
+            
