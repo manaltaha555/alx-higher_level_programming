efd01480701ed9c8036cc3f2f67f79e7141dad95
manaltaha555 <mnalth544@gmail.com>
2023-10-12 22:47:35 +0000

    hi

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -3,5 +3,5 @@
 def number_keys(a_dictionary):
     i = 0
     for key in a_dictionary:
-        i+=1
+        i += 1
     return i
---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    for x, y in a_dictionary.items().soretd():
+        print(x, ": ", y)
