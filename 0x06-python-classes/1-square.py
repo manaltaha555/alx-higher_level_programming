6b2b426dfcc1346160f5d669bda7869a770ecb12
manaltaha555 <mnalth544@gmail.com>
2023-10-24 16:24:09 -0700

    hshshhs

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""Square module"""
+"""
+Square module
+"""
 class Square:
-    """Defines a square"""
+    """
+    Defines a Square
+    """
     def __init__(self, size):
         """Constructor.
 
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Square module.
+"""
+class Square:
+    """
+    Defines a Square.
+    """
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: lenght of the square.
+        Raises:
+            TypeError: if size isn't an integer
+            ValueErro: if size si less than zero
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
