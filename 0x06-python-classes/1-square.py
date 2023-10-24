7f5f991facafad161d3510819b86090bf51768ab
manaltaha555 <mnalth544@gmail.com>
2023-10-24 15:54:11 -0700

    hshhsh

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """Define constructor"""
     def __init__(self, size):
+        """
+        Args:
+            size: lenght of the square
+        """
         self.__size = size
